4b200d1997447877b4f45228b980bda3eade6c55
Thorinka <mmlentyshenko@gmail.com>
2023-03-26 14:13:17 +0300

    add func_title and docstrings

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def func(user_input):
+    """Функция для замены всех строчных букв в строке на заглавные"""
     return user_input.upper()
 
+
+def func_title(user_input):
+    """Функция для замены первых букв в слове на заглавные"""
+    return user_input.title()
